38ea0c58c1dafaecf2b23243fdd7e32401e095b9
George Li <li.6795@osu.edu>
2018-11-15 20:33:51 -0500

    asdf

---
+++ words.py
@@ -0,0 +1,22 @@
+# assumes we take in numpy array [([genres], 'plot')]
+
+import pandas as pd
+# read in data (charles code)
+
+raw_data = pd.read_csv('movie_data.csv').values
+
+def transform_data(raw_data):
+  genres = raw_data[:,0]
+  for i, g in enumerate(genres):
+    genres[i] = g.split(':')
+
+  summaries = raw_data[:,1]
+  for ind, s in enumerate(summaries): 
+    sentences = str(s).split('.')
+    for i, sen in enumerate(sentences):
+      tokens = sen.split()
+      sentences[i] = tokens
+    summaries[ind] = sentences
+  return genres, summaries
+
+print transform_data(raw_data)
