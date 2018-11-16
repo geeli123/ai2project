11661b115721d99e713d2d1c3d4756e3c905c206
charlesyn <charlesyniu@gmail.com>
2018-11-15 21:29:00 -0500

    Add helper to get sample of data based on genre and size

--- MovieParse.py
+++ MovieParse.py
@@ -6,3 +6,6 @@ genres = {'action', 'horror', 'comedy', 'drama'}
 def getData(path):
     df = pd.read_csv(path)
     return df[df.Genres.isin(genres)]
+
+def getSample(df, genre, k):
+    return df[df.Genres.isin([genre])].sample(n=k)
